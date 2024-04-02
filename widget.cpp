e1e9dcf79641252923f78b51eff13c4927acd8de
GUERIN <marius.guerin75@gmail.com>
2024-04-02 15:01:13 +0000

    Upload New File

---
+++ widget.cpp
@@ -0,0 +1,106 @@
+#include "detectionpi.h"
+#include "opencv2/opencv.hpp"
+#include "widget.h"
+#include "ui_widget.h"
+#include <QString>
+#include <QTimer>
+
+
+using namespace cv;
+using std::cout;
+using std::endl;
+
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+
+    webCam_=new VideoCapture(0);
+    int width=webCam_->get(CAP_PROP_FRAME_WIDTH);
+    int height=webCam_->get(CAP_PROP_FRAME_HEIGHT);
+
+    if(!webCam_->isOpened())  // check if we succeeded
+    {
+        ui->infoLabel_->setText("Error openning the default camera !");
+    }
+    else
+    {
+        ui->infoLabel_->setText(QString("Video ok, image size is %1x%2 pixels").arg(width).arg(height));
+    }
+    timer_ = new QTimer(this);
+    connect(timer_, SIGNAL(timeout()), this, SLOT(on_captureButton__clicked()));
+    timer_->start(20);
+}
+
+
+Widget::~Widget()
+{
+    delete ui;
+    delete webCam_;
+}
+
+void Widget::on_captureButton__clicked()
+{
+    if (webCam_->isOpened()) {
+        Mat image;
+        if (webCam_->read(image)) {   // Capture a frame
+            // Flip to get a mirror effect
+            flip(image,image,1);
+            // Invert Blue and Red color channels
+            cvtColor(image,image,COLOR_BGR2RGB);
+            // Convert to Qt image
+            QImage img= QImage((const unsigned char*)(image.data),image.cols,image.rows,QImage::Format_RGB888);
+            // Display on label
+            ui->imageLabel_->setPixmap(QPixmap::fromImage(img));
+            // Resize the label to fit the image
+            ui->imageLabel_->resize(ui->imageLabel_->pixmap().size());
+
+            detectionpi(image);
+        }
+        else {
+            ui->infoLabel_->setText("Error capturing the frame");
+        }
+
+    }
+}
+
+void Widget::detectionpi(Mat img1){
+
+    Mat img2 = imread("../TestWebcam/carteE.JPG");
+    // img2 = resize(img2, img1.size());
+    //-- Step 1: Detect the keypoints using SIFT Detector, compute the descriptors
+    Ptr<SIFT> detector = SIFT::create( );
+    std::vector<KeyPoint> keypoints1, keypoints2;
+    Mat descriptors1, descriptors2;
+    detector->detectAndCompute( img1, noArray(), keypoints1, descriptors1 );
+    detector->detectAndCompute( img2, noArray(), keypoints2, descriptors2 );
+
+    //-- Step 2: Matching descriptor vectors with a FLANN based matcher
+    // Since SIFT is a floating-point descriptor NORM_L2 is used
+    Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create(DescriptorMatcher::FLANNBASED);
+    std::vector< std::vector<DMatch> > knn_matches;
+    matcher->knnMatch( descriptors1, descriptors2, knn_matches, 2 );
+
+    //-- Filter matches using the Lowe's ratio test
+    const float ratio_thresh = 0.7f;
+    std::vector<DMatch> good_matches;
+    for (size_t i = 0; i < knn_matches.size(); i++)
+    {
+        if (knn_matches[i][0].distance < ratio_thresh * knn_matches[i][1].distance)
+        {
+            good_matches.push_back(knn_matches[i][0]);
+        }
+    }
+
+    //-- Draw matches
+    Mat img_matches;
+    drawMatches( img1, keypoints1, img2, keypoints2, good_matches, img_matches, Scalar::all(-1),
+                Scalar::all(-1), std::vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
+
+    //-- Show detected matches
+    imshow("Good Matches", img_matches );
+
+    waitKey();
+
+}
